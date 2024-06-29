eb3e9b5c18db46a93ea9fb350c155652ddbcdf24
dillonph34 <e1156212@u.nus.edu>
2024-06-29 21:06:37 +0800

    fixed login

--- Finance360/app/firebaseConfig.js
+++ Finance360/app/firebaseConfig.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth} from 'firebase/auth';
+import {getAuth, initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import {AsyncStorage} from '@react-native-async-storage/async-storage';
 import {getFirestore} from 'firebase/firestore';
 import { getDatabase } from 'firebase/database'; 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -21,6 +22,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const FIREBASE_APP = initializeApp(firebaseConfig);
-export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
+export const FIREBASE_AUTH = initializeAuth(FIREBASE_APP, {
+  persistence: getReactNativePersistence(AsyncStorage)
+});
 export const FIRESTORE_DB = getFirestore(FIREBASE_APP);
 export const DATABASE = getDatabase(FIREBASE_APP);
